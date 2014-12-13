7e609165fe3fe12dc41a326fcddc892467969224
Shrikkanth Sreedharan <shrikkanth.sreedharan@gmail.com>
2014-12-13 23:25:44 +1300

    Camera following WIP: Camera follows player strictly.
    issue: camera
    Signed-off-by: Shrikkanth Sreedharan <shrikkanth.sreedharan@gmail.com>

--- 2DSideScroller/Assets/CharacterControl2D.cs
+++ 2DSideScroller/Assets/CharacterControl2D.cs
@@ -7,6 +7,7 @@ public class CharacterControl2D : MonoBehaviour {
 	CharacterController controller;
 	Animator animator;
 	Transform transform;
+	Camera mainCamera;
 
 	public float walkingSpeed = 0.1f;
 
@@ -15,6 +16,7 @@ public class CharacterControl2D : MonoBehaviour {
 		controller = GetComponent<CharacterController>();
 		animator = GetComponent<Animator>();
 		transform = GetComponent<Transform> ();
+		mainCamera = Camera.main;
 	}
 
 	CharacterDirection determineCharacterDirection ()
@@ -50,27 +52,37 @@ public class CharacterControl2D : MonoBehaviour {
 
 	void moveCharacter (CharacterDirection characterDirection)
 	{
+		var mainCameraTransform = mainCamera.transform;
+		Vector3 mainCamPosition = mainCameraTransform.localPosition;
+
 		switch (characterDirection) {
 			case CharacterDirection.LEFT: 
 				controller.Move (new Vector3 (-walkingSpeed, 0));
+				mainCamPosition.x += -walkingSpeed;
 				break;
 			case CharacterDirection.DOWNLEFT: 
 				controller.Move (new Vector3 (-walkingSpeed, 0));
+				mainCamPosition.x += -walkingSpeed;
 				break;
 			case CharacterDirection.UPLEFT: 
 				controller.Move (new Vector3 (-walkingSpeed, 0));
+				mainCamPosition.x += -walkingSpeed;
 				break;
 			case CharacterDirection.RIGHT: 
 				controller.Move (new Vector3 (walkingSpeed, 0));
+				mainCamPosition.x += walkingSpeed;
 				break;
 			case CharacterDirection.DOWNRIGHT: 
 				controller.Move (new Vector3 (walkingSpeed, 0));
+				mainCamPosition.x += walkingSpeed;
 				break;
 			case CharacterDirection.UPRIGHT: 
 				controller.Move (new Vector3 (walkingSpeed, 0));
+				mainCamPosition.x += walkingSpeed;
 				break;
-			default: return;	
+			default: break;	
 		}
+		mainCameraTransform.localPosition = mainCamPosition;
 
 	}
 
@@ -88,35 +100,35 @@ public class CharacterControl2D : MonoBehaviour {
 		}
 
 		switch (direction) {
-		case CharacterDirection.LEFT: 
-			animator.Play ("Archer1_Walk 0");
-			scaleXAxisOfCharacter(-1.0f);
-			break;
-		case CharacterDirection.DOWNLEFT:
-			animator.Play ("Archer1_Walk 0");
-			scaleXAxisOfCharacter(-1.0f);
-			break;
-		case CharacterDirection.UPLEFT:
-			animator.Play ("Archer1_Jump");
-			scaleXAxisOfCharacter(-1.0f);
-			break;
-		case CharacterDirection.RIGHT:
-			animator.Play ("Archer1_Walk 0");
-			scaleXAxisOfCharacter(1.0f);
-			break;
-		case CharacterDirection.DOWNRIGHT:
-			animator.Play ("Archer1_Walk 0");
-			scaleXAxisOfCharacter(1.0f);
-			break;
-		case CharacterDirection.UPRIGHT:
-			animator.Play ("Archer1_Jump");
-			scaleXAxisOfCharacter(1.0f);
-			break;
-		case CharacterDirection.UP:
-			animator.Play ("Archer1_Jump");
-			break;
-		default: 
-			animator.Play ("Archer1_Idle");
+			case CharacterDirection.LEFT: 
+				animator.Play ("Archer1_Walk 0");
+				scaleXAxisOfCharacter(-1.0f);
+				break;
+			case CharacterDirection.DOWNLEFT:
+				animator.Play ("Archer1_Walk 0");
+				scaleXAxisOfCharacter(-1.0f);
+				break;
+			case CharacterDirection.UPLEFT:
+				animator.Play ("Archer1_Jump");
+				scaleXAxisOfCharacter(-1.0f);
+				break;
+			case CharacterDirection.RIGHT:
+				animator.Play ("Archer1_Walk 0");
+				scaleXAxisOfCharacter(1.0f);
+				break;
+			case CharacterDirection.DOWNRIGHT:
+				animator.Play ("Archer1_Walk 0");
+				scaleXAxisOfCharacter(1.0f);
+				break;
+			case CharacterDirection.UPRIGHT:
+				animator.Play ("Archer1_Jump");
+				scaleXAxisOfCharacter(1.0f);
+				break;
+			case CharacterDirection.UP:
+				animator.Play ("Archer1_Jump");
+				break;
+			default: 
+				animator.Play ("Archer1_Idle");
 			return;
 		}
 		
