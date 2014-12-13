996bd4c07182e3bebb2320df7c82a895162cb1a0
Shrikkanth Sreedharan <shrikkanth.sreedharan@gmail.com>
2014-12-13 19:44:51 +1300

    Got character moving
    issue: character
    Signed-off-by: Shrikkanth Sreedharan <shrikkanth.sreedharan@gmail.com>

---
+++ 2DSideScroller/Assets/CharacterControl2D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class CharacterControl2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+		controller.Move(new Vector3(1,0));
+
+	}
+}
