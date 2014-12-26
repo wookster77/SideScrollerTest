998db75c2fa40670c29393d531296435a178ac2d
Lochana Wijesundera <lochana_nerd@hotmail.com>
2014-12-26 20:21:14 +1300

    Parallax working using ParallaxController. Sped up character animation.
    ISSUE: Parallax

---
+++ 2DSideScroller/Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxController : MonoBehaviour {
+
+	Transform backgroundTransform;
+	Camera mainCamera;
+	Vector3 cameraPreviousPosition;
+	Vector3 cameraCurrentPosition;
+	public float cameraFollowPercentage = 100.0f;
+	Vector3 cameraVelocity;
+	// Use this for initialization
+	void Start () {
+		backgroundTransform = GetComponent<Transform> ();
+		mainCamera = Camera.main;
+		cameraPreviousPosition = mainCamera.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cameraVelocity = GetCameraVelocity ();
+		Vector3 backgroundPosition = backgroundTransform.position;
+		backgroundPosition.x += cameraVelocity.x*(cameraFollowPercentage/100.0f);
+		backgroundTransform.position = backgroundPosition;
+	}
+
+	Vector3 GetCameraVelocity()
+	{
+		cameraCurrentPosition = mainCamera.transform.position;
+		cameraVelocity = cameraCurrentPosition - cameraPreviousPosition;
+		cameraPreviousPosition = cameraCurrentPosition;
+		return cameraVelocity;
+	}
+}
